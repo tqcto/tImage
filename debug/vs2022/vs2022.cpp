a8fea4f6ff67ce8047edbf4f5cbee9927e3dace5
tqcto <lol.scald.lol@gmail.com>
2026-09-04 04:40:04 +0900

    add test project in vs2022

---
+++ debug/vs2022/vs2022.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+#include <tImage.h>
+
+#define IMG_PATH "F:\\SS\\9512.png"
+
+int main(void) {
+
+    tImage::Image src;
+    tImage::decodePNG(&src, IMG_PATH);
+
+    tImage::encodePNG(&src, "test.png");
+    
+    return 0;
+
+}
