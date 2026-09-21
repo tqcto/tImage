10862af0ccdfbc5b92c17e1f646adaf78bf052c8
tqcto <lol.scald.lol@gmail.com>
2026-09-21 23:06:26 +0900

    update split and merge functions

--- src/tool/merge.cpp
+++ src/tool/merge.cpp
@@ -6,6 +6,32 @@
 
 namespace tImage {
 
+    void merge_RGB24(Image* src_planes, Image* dst) {
+
+		const t_int width = dst->width();
+        const t_int height = dst->height();
+
+		#pragma omp parallel for schedule(static) if(width * height >= 512 * 512)
+		for (t_int y = 0; y < height; y++) {
+
+            t_uchar* src0_rowptr = src_planes[0].rowPtr(y);
+            t_uchar* src1_rowptr = src_planes[1].rowPtr(y);
+            t_uchar* src2_rowptr = src_planes[2].rowPtr(y);
+			t_uchar* dst_rowptr = dst->rowPtr(y);
+
+			// #pragma omp simd
+			for (t_int x = 0; x < width; x++) {
+
+				dst_rowptr[x * 3] = src0_rowptr[x];
+				dst_rowptr[x * 3 + 1] = src1_rowptr[x];
+				dst_rowptr[x * 3 + 2] = src2_rowptr[x];
+
+			}
+
+		}
+
+	}
+
 	void merge_RGBA32(Image* src_planes, Image* dst) {
 
 		const t_int width = dst->width();
@@ -34,6 +60,30 @@ namespace tImage {
 
 	}
 
+    void merge_generic(Image* src_planes, Image* dst) {
+
+		const t_int width = dst->width();
+        const t_int height = dst->height();
+        const t_int channels = dst->channels();
+
+		#pragma omp parallel for schedule(static) if(width * height >= 512 * 512)
+		for (t_int y = 0; y < height; y++) {
+
+			t_uchar* dst_rowptr = dst->rowPtr(y);
+
+			for (t_int x = 0; x < width; x++) {
+                #pragma omp simd
+                for (t_int c = 0; c < channels; c++) {
+
+                    dst_rowptr[x * channels + c] = src_planes[c].rowPtr(y)[x];
+
+                }
+			}
+
+		}
+
+	}
+
     t_err merge(Image* src_planes, Image* dst) {
 
 		if (!src_planes || !dst || dst->channels() < 2) return t_err_InvalidArgument;
@@ -42,17 +92,16 @@ namespace tImage {
 
         // RGB24
         if (channels == 3) {
-            //merge_RGB24(src_planes, dst);
+            merge_RGB24(src_planes, dst);
         }
         // RGBA32
         else if (channels == 4) {
             merge_RGBA32(src_planes, dst);
         }
-        /*
         // other
         else {
-            return split_generic(src_planes, dst);
-        }*/
+            merge_generic(src_planes, dst);
+        }
 
 		return t_err_None;
 
--- src/tool/split.cpp
+++ src/tool/split.cpp
@@ -31,56 +31,82 @@ namespace tImage {
 	}
     */
 
+    // void split_RGB24(Image* src, Image* dst_planes) {
+
+    //     t_uint64 total_pixels = (t_uint64)src->width() * src->height();
+    //     // バッファに余裕があると仮定（allocate時に padding を追加）
+
+    //     t_uchar* src_data = src->data;
+    //     t_uchar* dst_r = dst_planes[0].data;
+    //     t_uchar* dst_g = dst_planes[1].data;
+    //     t_uchar* dst_b = dst_planes[2].data;
+
+    //     #if defined(__AVX2__)
+    //     // AVX2: 32ピクセル（96バイト）単位で全ピクセルを処理
+    //     // ストライド超過分は無視（バッファ末尾の padding で吸収）
+    //     for (t_uint64 i = 0; i < total_pixels; i += 32) {
+    //         __m256i block0 = _mm256_loadu_si256((const __m256i*)(src_data + 3*i + 0));
+    //         __m256i block1 = _mm256_loadu_si256((const __m256i*)(src_data + 3*i + 32));
+    //         __m256i block2 = _mm256_loadu_si256((const __m256i*)(src_data + 3*i + 64));
+
+    //         // PSHUFB（256bit レーン毎に 128bit 単位で実行）
+    //         const __m256i shuffle_r = _mm256_setr_epi8(
+    //             0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, -1, -1, -1, -1, -1,
+    //             0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, -1, -1, -1, -1, -1
+    //         );
+    //         const __m256i shuffle_g = _mm256_setr_epi8(
+    //             1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, -1, -1, -1, -1, -1,
+    //             1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, -1, -1, -1, -1, -1
+    //         );
+    //         const __m256i shuffle_b = _mm256_setr_epi8(
+    //             2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, -1, -1, -1, -1, -1,
+    //             2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, -1, -1, -1, -1, -1
+    //         );
+            
+    //         __m256i r = _mm256_shuffle_epi8(block0, shuffle_r);
+    //         __m256i g = _mm256_shuffle_epi8(block1, shuffle_g);
+    //         __m256i b = _mm256_shuffle_epi8(block2, shuffle_b);
+
+    //         _mm256_storeu_si256((__m256i*)(dst_r + i), r);
+    //         _mm256_storeu_si256((__m256i*)(dst_g + i), g);
+    //         _mm256_storeu_si256((__m256i*)(dst_b + i), b);
+    //     }
+    //     #else
+    //     for (t_uint64 i = 0; i < total_pixels; ++i) {
+    //         dst_r[i] = src_data[3 * i + 0];
+    //         dst_g[i] = src_data[3 * i + 1];
+    //         dst_b[i] = src_data[3 * i + 2];
+    //     }
+    //     #endif
+
+    // }
+
     void split_RGB24(Image* src, Image* dst_planes) {
 
-        t_uint64 total_pixels = (t_uint64)src->width() * src->height();
-        // バッファに余裕があると仮定（allocate時に padding を追加）
+        const t_int width = src->width();
+        const t_int height = src->height();
 
-        t_uchar* src_data = src->data;
-        t_uchar* dst_r = dst_planes[0].data;
-        t_uchar* dst_g = dst_planes[1].data;
-        t_uchar* dst_b = dst_planes[2].data;
+        #pragma omp parallel for schedule(static) if(width * height >= 512 * 512)
+        for (t_int y = 0; y < height; y++) {
 
-        #if defined(__AVX2__)
-        // AVX2: 32ピクセル（96バイト）単位で全ピクセルを処理
-        // ストライド超過分は無視（バッファ末尾の padding で吸収）
-        for (t_uint64 i = 0; i < total_pixels; i += 32) {
-            __m256i block0 = _mm256_loadu_si256((const __m256i*)(src_data + 3*i + 0));
-            __m256i block1 = _mm256_loadu_si256((const __m256i*)(src_data + 3*i + 32));
-            __m256i block2 = _mm256_loadu_si256((const __m256i*)(src_data + 3*i + 64));
-
-            // PSHUFB（256bit レーン毎に 128bit 単位で実行）
-            const __m256i shuffle_r = _mm256_setr_epi8(
-                0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, -1, -1, -1, -1, -1,
-                0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, -1, -1, -1, -1, -1
-            );
-            const __m256i shuffle_g = _mm256_setr_epi8(
-                1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, -1, -1, -1, -1, -1,
-                1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, -1, -1, -1, -1, -1
-            );
-            const __m256i shuffle_b = _mm256_setr_epi8(
-                2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, -1, -1, -1, -1, -1,
-                2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, -1, -1, -1, -1, -1
-            );
+            t_uchar* src_rowptr = src->rowPtr(y);
+            t_uchar* dst0_rowptr = dst_planes[0].rowPtr(y);
+            t_uchar* dst1_rowptr = dst_planes[1].rowPtr(y);
+            t_uchar* dst2_rowptr = dst_planes[2].rowPtr(y);
             
-            __m256i r = _mm256_shuffle_epi8(block0, shuffle_r);
-            __m256i g = _mm256_shuffle_epi8(block1, shuffle_g);
-            __m256i b = _mm256_shuffle_epi8(block2, shuffle_b);
+            // #pragma omp simd
+            for (t_int x = 0; x < width; x++) {
 
-            _mm256_storeu_si256((__m256i*)(dst_r + i), r);
-            _mm256_storeu_si256((__m256i*)(dst_g + i), g);
-            _mm256_storeu_si256((__m256i*)(dst_b + i), b);
+                dst0_rowptr[x] = src_rowptr[x * 3];
+                dst1_rowptr[x] = src_rowptr[x * 3 + 1];
+                dst2_rowptr[x] = src_rowptr[x * 3 + 2];
+
+            }
+
         }
-        #else
-        for (t_uint64 i = 0; i < total_pixels; ++i) {
-            dst_r[i] = src_data[3 * i + 0];
-            dst_g[i] = src_data[3 * i + 1];
-            dst_b[i] = src_data[3 * i + 2];
-        }
-        #endif
 
     }
-
+    
     void split_RGBA32(Image* src, Image* dst_planes) {
 
         const t_int width = src->width();
@@ -109,6 +135,30 @@ namespace tImage {
 
     }
 
+    void split_generic(Image* src, Image* dst_planes) {
+
+        const t_int width = src->width();
+        const t_int height = src->height();
+        const t_int channels = src->channels();
+
+        #pragma omp parallel for schedule(static) if(width * height >= 512 * 512)
+        for (t_int y = 0; y < height; y++) {
+
+            t_uchar* src_rowptr = src->rowPtr(y);
+
+            for (t_int x = 0; x < width; x++) {
+                #pragma omp simd
+                for (t_int c = 0; c < channels; c++) {
+
+                    dst_planes[c].rowPtr(y)[x] = src_rowptr[x * channels + c];
+
+                }
+            }
+
+        }
+
+    }
+
     t_err split(Image* src, Image* dst_planes) {
 
         if (!src || src->empty() || src->channels() < 2) return t_err_InvalidArgument;
@@ -123,11 +173,10 @@ namespace tImage {
         else if (channels == 4) {
             split_RGBA32(src, dst_planes);
         }
-        /*
         // other
         else {
-            return split_generic(src, dst_planes);
-        }*/
+            split_generic(src, dst_planes);
+        }
 
         return t_err_None;
 
