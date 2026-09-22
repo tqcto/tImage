faed8bd85ceeeef4b328904ac201cf7868704fda
tqcto <lol.scald.lol@gmail.com>
2026-09-23 00:40:47 +0900

    add core

---
+++ include/core/mem/alloc.h
@@ -0,0 +1,36 @@
+#include "../../tImage_definition.h"
+
+#if defined(_MSC_VER) || defined(__MINGW32__)
+#include <malloc.h>
+#else
+#include <cstdlib>
+#endif
+
+namespace tImage {
+namespace core {
+namespace mem {
+
+    // allocate aligned memory
+    template<typename T = void>
+    inline T* alignedMalloc(
+        t_uint64 bytes, t_uint64 alignment = alignof(T)
+    ) noexcept {
+
+        #if defined(_MSC_VER) || defined(__MINGW32__)
+        
+        return reinterpret_cast<T*>(_aligned_malloc(bytes, alignment));
+        
+        #else
+
+        void* p = nullptr;
+        posix_memalign(&p, alignment, bytes);
+        
+        return p;
+        
+        #endif
+
+    }
+
+}
+}
+}
---
+++ include/core/mem/release.h
@@ -0,0 +1,30 @@
+#include "../../tImage_definition.h"
+
+#if defined(_MSC_VER) || defined(__MINGW32__)
+#include <malloc.h>
+#else
+#include <cstdlib>
+#endif
+
+namespace tImage {
+namespace core {
+namespace mem {
+
+    // release aligned memory
+    inline void alignedFree(void* ptr) {
+
+        #if defined(_MSC_VER) || defined(__MINGW32__)
+        
+        _aligned_free(ptr);
+        
+        #else
+
+        std::free(ptr);
+        
+        #endif
+
+    }
+
+}
+}
+}
