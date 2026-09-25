63312fb37e795374c390343795c91377dd4bd9c2
tqcto <lol.scald.lol@gmail.com>
2026-09-26 05:27:40 +0900

    update swap

--- src/tool/swap.cpp
+++ src/tool/swap.cpp
@@ -7,6 +7,81 @@
 
 namespace tImage {
 
+    // is mem_align > check_align
+    inline t_bool check_big_align(t_uint mem_align, t_uint check_align) {
+        return mem_align & (mem_align - 1) &&  mem_align > check_align;
+    }
+
+    inline void swap3(Image* src, Image* dst) {
+
+        auto proc = core::t_CPU_INFO.processor;
+
+        const t_int width = src->width();
+        const t_int height = src->height();
+
+        // check aligned
+        const t_bool check_aligned = check_big_align(src->align(), 16) && check_big_align(dst->align(), 16);
+
+        // simd 128 bit
+        if (proc & core::t_cpu_processor_SSSE3 && check_aligned) {
+
+            const t_int loop = 3 * (width >> 4);
+
+            const core::simd::v_uint8x16 mask(
+                2, 1, 0,
+                5, 4, 3,
+                8, 7, 6,
+                11, 10, 9,
+                14, 13, 12,
+                15
+            );
+
+            #pragma omp parallel for
+            for (t_int y = 0; y < height; y++) {
+
+                core::simd::v_uint8x16 src_data;
+
+                auto src_rowptr = src->rowPtr(y);
+                auto dst_rowptr = dst->rowPtr(y);
+
+                for (t_int x = 0; x < loop; x++) {
+                    
+                    core::simd::v128_load_8x16(&src_rowptr[x * 5], src_data);
+
+                    core::simd::v128_shuffle_8x16(src_data, src_data, mask);
+
+                    core::simd::v128_store_8x16(src_data, &dst_rowptr[x * 5]);
+
+                }
+
+            }
+
+        }
+        // normal
+        else {
+
+            #pragma omp parallel for
+            for (t_int y = 0; y < height; y++) {
+
+                auto src_rowptr = src->rowPtr(y);
+                auto dst_rowptr = dst->rowPtr(y);
+
+                for (t_int x = 0; x < width; x++) {
+
+                    const t_int i = x * 3;
+
+                    dst_rowptr[i] = src_rowptr[i + 2];
+                    dst_rowptr[i + 1] = src_rowptr[i + 1];
+                    dst_rowptr[i + 2] = src_rowptr[i];
+
+                }
+
+            }
+
+        }
+
+    }
+
     inline void swap4(Image* src, Image* dst) {
 
         auto proc = core::t_CPU_INFO.processor;
@@ -14,10 +89,13 @@ namespace tImage {
         const t_int width = src->width();
         const t_int height = src->height();
 
-        // simd 256 bit
-        if (proc & core::t_cpu_processor_AVX) {
+        // check aligned
+        const t_bool check_aligned = check_big_align(src->align(), 32) && check_big_align(dst->align(), 32);
 
-            const t_int loop_count = src->elementsRow() >> 5;
+        // simd 256 bit
+        if (proc & core::t_cpu_processor_AVX && check_aligned) {
+
+            const t_int loop = src->elementsRow() >> 5;
 
             const core::simd::v_uint8x32 mask(
                 2, 1, 0, 3,
@@ -38,7 +116,7 @@ namespace tImage {
                 auto src_rowptr = src->rowPtr(y);
                 auto dst_rowptr = dst->rowPtr(y);
 
-                for (t_int x = 0; x < loop_count; x++) {
+                for (t_int x = 0; x < loop; x++) {
                     
                     core::simd::v256_load_8x32(&src_rowptr[x << 5], src_data);
 
@@ -52,9 +130,9 @@ namespace tImage {
 
         }
         // simd 128 bit
-        if (proc & core::t_cpu_processor_SSSE3) {
+        if (proc & core::t_cpu_processor_SSSE3 && check_aligned) {
 
-            const t_int loop_count = src->elementsRow() >> 2;
+            const t_int loop = src->elementsRow() >> 2;
 
             const core::simd::v_uint8x16 mask(
                 2, 1, 0, 3,
@@ -71,7 +149,7 @@ namespace tImage {
                 auto src_rowptr = src->rowPtr(y);
                 auto dst_rowptr = dst->rowPtr(y);
 
-                for (t_int x = 0; x < loop_count; x++) {
+                for (t_int x = 0; x < loop; x++) {
                     
                     core::simd::v128_load_8x16(&src_rowptr[x << 2], src_data);
 
@@ -86,7 +164,7 @@ namespace tImage {
         // normal
         else {
 
-            //#pragma omp parallel for
+            #pragma omp parallel for
             for (t_int y = 0; y < height; y++) {
 
                 auto src_rowptr = src->rowPtr(y);
@@ -116,7 +194,7 @@ namespace tImage {
         switch (src->channels())
         {
         case 3:
-        
+            swap3(src, dst);
             break;
         
         case 4:
@@ -124,7 +202,8 @@ namespace tImage {
             break;
 
         default:
-            break;
+            return t_err_InvalidArgument;    
+        
         }
 
         return t_err_None;
